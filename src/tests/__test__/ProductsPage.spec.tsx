2cf77318bd7d25307080c33f0fa838baaa493b55
Luis del Amo <luisdelamo@gmail.com>
2025-03-09 22:14:25 +0100

    mock products mws

--- src/tests/__test__/ProductsPage.spec.tsx
+++ src/tests/__test__/ProductsPage.spec.tsx
@@ -1,15 +1,39 @@
 import { render, RenderResult, screen } from "@testing-library/react";
-import { test, expect } from "vitest";
+import { test, expect, describe, beforeAll, afterAll, afterEach } from "vitest";
 import { ProductsPage } from "../../pages/ProductsPage.tsx";
 import { AppProvider } from "../../context/AppProvider.tsx";
 import { ReactNode } from "react";
+import { MockWebServer } from "../MockWebServer.ts";
+import productsResponse from "./data/productsResponse.json";
 
-test("Loads and displays title", () => {
-    renderComponent(<ProductsPage />);
+const mockWebServer = new MockWebServer();
 
-    const titleElement = screen.getByRole("heading", { name: /product price updater/i });
-    expect(titleElement).toBeInTheDocument();
-});
+
+describe("Products Page component", () => {
+    beforeAll(() => mockWebServer.start());
+    afterEach(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
+    test("Loads and displays title", () => {
+        givenProducts();
+        renderComponent(<ProductsPage />);
+
+        const titleElement = screen.getByRole("heading", { name: /product price updater/i });
+        expect(titleElement).toBeInTheDocument();
+    });
+
+})
+
+function givenProducts() {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            response: productsResponse,
+            httpStatusCode: 200,
+        },
+    ]);
+}
+
 
 function renderComponent(component: ReactNode): RenderResult {
     return render(<AppProvider>{component}</AppProvider>);
